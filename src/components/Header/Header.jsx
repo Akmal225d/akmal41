38b28613fa471f476eaf38b0d90d81b0f8b7134c
Akmal225d <islombahodirov424@gmail.com>
2024-11-22 16:57:06 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+const Header = () => {
+  return (
+   <>
+<nav>
+    <Link to={'/'}>Home</Link>
+    <Link to={'/About'}>About</Link>
+</nav>
+
+   </>
+  )
+}
+
+STARTER-REACT-ROUTER-DOM
+
+export default Header
